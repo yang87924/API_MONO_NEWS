7ee4a63da4636a6cdef39284abcf4b638846f922
yang <yang@gmail.com>
2022-10-11 21:33:20 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
 function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch(process.env.REACT_APP_MY_ENV_VARIABLE)
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data.data);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" bg-yellow-600 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="  bg-white  mx-auto max-w-3xl">
+        <ul role="list" className="divide-y divide-gray-500">
+          {posts.map((post) => {
+            return (
+              <li className="px-4 py-4 sm:px-6">
+                <h2 className="post-title">{post.title}</h2>
+                <h2 className="text-right">{post.date}</h2>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
-
 export default App;
